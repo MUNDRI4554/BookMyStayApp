b3df636bf0b93957c0c254a77b977780862e0b12
MUNDRI4554 <adityaanumod@gmail.com>
2026-03-25 09:01:52 +0530

    02 UC 1 - Application Entry & Welcome Message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,16 @@
+public class BookMyStayApp
+{
+
+    public static void main(String[] args) {
+
+        String appName = "Book My Stay - Hotel Booking System";
+        String version = "Version 1.0";
+
+        System.out.println("======================================");
+        System.out.println(" Welcome to " + appName);
+        System.out.println(" " + version);
+        System.out.println("======================================");
+        System.out.println("Your comfortable stay begins here!");
+        System.out.println("======================================");
+    }
+}
